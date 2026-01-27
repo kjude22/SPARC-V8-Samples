eb047c222094f488eff8d54d0926cd49aeb40cb8
KIM KWAN <81316934+kjude22@users.noreply.github.com>
2026-01-27 17:59:09 +0900

    Add files via upload
    SPARC 실습용 IU 샘플 폴더 6개

---
+++ IU/IU_y-register-example/b-gr740/mulDiv.d
@@ -0,0 +1,223 @@
+b-gr740/mulDiv.o: mulDiv.c \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/config.h \
+ /opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stddef.h \
+ /opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stdint.h \
+ /opt/rtems/6/sparc-rtems6/include/stdint.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/_default_types.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/features.h \
+ /opt/rtems/6/sparc-rtems6/include/_newlib_version.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_intsup.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_stdint.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/config.h \
+ /opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stdbool.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/tasks.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/cpuset.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/cdefs.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_cpuset.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_bitset.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/bitset.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_types.h \
+ /opt/rtems/6/sparc-rtems6/include/newlib.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/config.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/ieeefp.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/_types.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/_bitcount.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/attr.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/modes.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/cpu.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/basedefs.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/cpuopts.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/sparc.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/status.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/types.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_timespec.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_timeval.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/object.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/watchdogticks.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/context.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/stack.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/isr.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/isrlevel.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/assert.h \
+ /opt/rtems/6/sparc-rtems6/include/assert.h \
+ /opt/rtems/6/sparc-rtems6/include/_ansi.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/memory.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smp.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/threadidledata.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/userextdata.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/userext.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/interr.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/chain.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/wkspacedata.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/extension.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/fatal.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/init.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/io.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/barrier.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/cache.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/clock.h \
+ /opt/rtems/6/sparc-rtems6/include/time.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/reent.h \
+ /opt/rtems/6/sparc-rtems6/include/_ansi.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/lock.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/time.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/types.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/endian.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/_endian.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/select.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_sigset.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/timespec.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_pthreadtypes.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/sched.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/types.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/_locale.h \
+ /opt/rtems/6/sparc-rtems6/include/signal.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/signal.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/dpmem.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/event.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/options.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/intr.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/isrlock.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smplock.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smplockstats.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smplockticket.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/atomic.h \
+ /opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stdatomic.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/message.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/coremsgbuffer.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/object.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/part.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/ratemon.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/region.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/heapinfo.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/scheduler.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/sem.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/signal.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/asr.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/support.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/timer.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/test.h \
+ /opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stdarg.h \
+ /opt/rtems/6/sparc-rtems6/include/string.h \
+ /opt/rtems/6/sparc-rtems6/include/strings.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/string.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/test-info.h \
+ /opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smpbarrier.h \
+ /opt/rtems/6/sparc-rtems6/include/stdlib.h \
+ /opt/rtems/6/sparc-rtems6/include/machine/stdlib.h \
+ /opt/rtems/6/sparc-rtems6/include/alloca.h \
+ /workspace/samples/utils/include/buffer_test_io.h \
+ /opt/rtems/6/sparc-rtems6/include/stdio.h \
+ /opt/rtems/6/sparc-rtems6/include/sys/stdio.h
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/config.h:
+/opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stddef.h:
+/opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stdint.h:
+/opt/rtems/6/sparc-rtems6/include/stdint.h:
+/opt/rtems/6/sparc-rtems6/include/machine/_default_types.h:
+/opt/rtems/6/sparc-rtems6/include/sys/features.h:
+/opt/rtems/6/sparc-rtems6/include/_newlib_version.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_intsup.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_stdint.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/config.h:
+/opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stdbool.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/tasks.h:
+/opt/rtems/6/sparc-rtems6/include/sys/cpuset.h:
+/opt/rtems/6/sparc-rtems6/include/sys/cdefs.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_cpuset.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_bitset.h:
+/opt/rtems/6/sparc-rtems6/include/sys/bitset.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_types.h:
+/opt/rtems/6/sparc-rtems6/include/newlib.h:
+/opt/rtems/6/sparc-rtems6/include/sys/config.h:
+/opt/rtems/6/sparc-rtems6/include/machine/ieeefp.h:
+/opt/rtems/6/sparc-rtems6/include/machine/_types.h:
+/opt/rtems/6/sparc-rtems6/include/machine/_bitcount.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/attr.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/modes.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/cpu.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/basedefs.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/cpuopts.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/sparc.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/status.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/types.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_timespec.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_timeval.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/object.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/watchdogticks.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/context.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/stack.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/isr.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/isrlevel.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/assert.h:
+/opt/rtems/6/sparc-rtems6/include/assert.h:
+/opt/rtems/6/sparc-rtems6/include/_ansi.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/memory.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smp.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/threadidledata.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/userextdata.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/userext.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/interr.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/chain.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/wkspacedata.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/extension.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/fatal.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/init.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/io.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/barrier.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/cache.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/clock.h:
+/opt/rtems/6/sparc-rtems6/include/time.h:
+/opt/rtems/6/sparc-rtems6/include/sys/reent.h:
+/opt/rtems/6/sparc-rtems6/include/_ansi.h:
+/opt/rtems/6/sparc-rtems6/include/sys/lock.h:
+/opt/rtems/6/sparc-rtems6/include/machine/time.h:
+/opt/rtems/6/sparc-rtems6/include/sys/types.h:
+/opt/rtems/6/sparc-rtems6/include/machine/endian.h:
+/opt/rtems/6/sparc-rtems6/include/machine/_endian.h:
+/opt/rtems/6/sparc-rtems6/include/sys/select.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_sigset.h:
+/opt/rtems/6/sparc-rtems6/include/sys/timespec.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_pthreadtypes.h:
+/opt/rtems/6/sparc-rtems6/include/sys/sched.h:
+/opt/rtems/6/sparc-rtems6/include/machine/types.h:
+/opt/rtems/6/sparc-rtems6/include/sys/_locale.h:
+/opt/rtems/6/sparc-rtems6/include/signal.h:
+/opt/rtems/6/sparc-rtems6/include/sys/signal.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/dpmem.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/event.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/options.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/intr.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/isrlock.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smplock.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smplockstats.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smplockticket.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/atomic.h:
+/opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stdatomic.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/message.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/coremsgbuffer.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/object.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/part.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/ratemon.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/region.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/heapinfo.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/scheduler.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/sem.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/signal.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/asr.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/support.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/rtems/timer.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/test.h:
+/opt/rtems/6/lib/gcc/sparc-rtems6/13.3.0/include/stdarg.h:
+/opt/rtems/6/sparc-rtems6/include/string.h:
+/opt/rtems/6/sparc-rtems6/include/strings.h:
+/opt/rtems/6/sparc-rtems6/include/sys/string.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/test-info.h:
+/opt/rtems/6/sparc-rtems6/gr740/lib/include/rtems/score/smpbarrier.h:
+/opt/rtems/6/sparc-rtems6/include/stdlib.h:
+/opt/rtems/6/sparc-rtems6/include/machine/stdlib.h:
+/opt/rtems/6/sparc-rtems6/include/alloca.h:
+/workspace/samples/utils/include/buffer_test_io.h:
+/opt/rtems/6/sparc-rtems6/include/stdio.h:
+/opt/rtems/6/sparc-rtems6/include/sys/stdio.h:
